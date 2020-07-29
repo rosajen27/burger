3bfaab1cb03b27292fe9c9598bd37f636a9f1ab0
Jennifer Rosa <rosajen27@gmail.com>
2020-07-29 11:58:01 -0400

    create db sql files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (id, burger_name, devoured) VALUES (1, 'Cheeseburger', false);
+INSERT INTO burgers (id, burger_name, devoured) VALUES (2, 'Hamburger', false);
+INSERT INTO burgers (id, burger_name, devoured) VALUES (3, 'Bacon Cheeseburger', true);
